8df6c3be189a9d414b3070e66ae602ffa2d83936
Matevž Poljanc <mp775@cam.ac.uk>
2018-02-13 14:56:09 +0000

    Update .travis-ci.sh

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,9 +1,11 @@
 # Edit this for your own project dependencies
 OPAM_DEPENDS="ocamlfind incremental ppx_driver=v0.9.2 ppx_jane=v0.9.0 ounit re ezirmin jbuilder core ocaml-migrate-parsetree"
 	 
-echo "yes" | sudo add-apt-repository ppa:avsm/ppa
-sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+# echo "yes" | sudo add-apt-repository ppa:avsm/ppa
+sudo apt-get update
+sudo apt-get install ocaml ocaml-native-compilers camlp4-extra opam
+
+opam switch 4.05.0
 
 export OPAMYES=1
 opam init 
